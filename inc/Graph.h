b4389819f62ac0e5704cd7361d6e2cbec3c1c21b
Kavin <kavins.iitm@gmail.com>
2016-09-30 00:27:42 +0530

    implemented fourSweep() and compiled; yet to test

--- inc/Graph.h
+++ inc/Graph.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <set>
 #include <queue>
+#include <utility>
 
 using namespace std;
 
@@ -12,13 +13,18 @@ class Graph {
   int numberOfEdges;
   vector< vector<int> > adjList;
 
+  vector<int> getDistanceFromU(int u);
   int computeEccentricity(int u);
   vector< set<int> > computeF(int u);
   int computeMaxEccentricity(set<int> vertices);
+  int getVertexWithMaximumDegree();
+  int maxDistantVertex(int u);
+  int midVertex(int u, int v);
 
   public:
   Graph(int numberOfVertices);
   int iFUB(int u, int l, int k);
+  pair<int, int> fourSweep();
 };
 
 #endif
--- src/Graph.cpp
+++ src/Graph.cpp
@@ -5,7 +5,7 @@ Graph::Graph(int numberOfVertices) {
   adjList.reserve(numberOfVertices);
 }
 
-int Graph::computeEccentricity(int u) {
+vector<int> Graph::getDistanceFromU(int u) {
   vector<int> distanceFromU(numberOfVertices);
   vector<bool> visited(numberOfVertices, false);
   queue<int> BFSQueue;
@@ -14,7 +14,6 @@ int Graph::computeEccentricity(int u) {
   distanceFromU[u] = 0;
   BFSQueue.push(u);
 
-  int eccentricity = 0;
   while (!BFSQueue.empty()) {
     int top = BFSQueue.front();
     BFSQueue.pop();
@@ -23,42 +22,27 @@ int Graph::computeEccentricity(int u) {
       if (!visited[*it]) {
         visited[*it] = true;
         distanceFromU[*it] = distanceFromU[top] + 1;
-        if (distanceFromU[*it] > eccentricity) {
-          eccentricity = distanceFromU[*it];
-        }
         BFSQueue.push(*it);
       }
+    }
+  }
+  return distanceFromU;
+}
+
+int Graph::computeEccentricity(int u) {
+  vector<int> distanceFromU = getDistanceFromU(u);
+  int eccentricity = 0;
+  for (vector<int>::iterator it=distanceFromU.begin(); it!=distanceFromU.end(); ++it) {
+    if (*it > eccentricity) {
+      eccentricity = *it;
     }
   }
   return eccentricity;
 }
 
 vector< set<int> > Graph::computeF(int u) {
-  vector<int> distanceFromU(numberOfVertices);
-  vector<bool> visited(numberOfVertices, false);
-  queue<int> BFSQueue;
-
-  visited[u] = true;
-  distanceFromU[u] = 0;
-  BFSQueue.push(u);
-
-  int eccentricity = 0;
-  while (!BFSQueue.empty()) {
-    int top = BFSQueue.front();
-    BFSQueue.pop();
-
-    for (vector<int>::iterator it=adjList[top].begin(); it!=adjList[top].end(); ++it) {
-      if (!visited[*it]) {
-        visited[*it] = true;
-        distanceFromU[*it] = distanceFromU[top] + 1;
-        if (distanceFromU[*it] > eccentricity) {
-          eccentricity = distanceFromU[*it];
-        }
-        BFSQueue.push(*it);
-      }
-    }
-  }
-
+  vector<int> distanceFromU = getDistanceFromU(u);
+  int eccentricity = computeEccentricity(u);
   vector< set<int> > F(eccentricity+1);
   for (int i=0; i<numberOfVertices; ++i) {
     F[distanceFromU[i]].insert(i);
@@ -94,4 +78,77 @@ int Graph::iFUB(int u, int l, int k) {
     }
     i--;
   }
+}
+
+int Graph::getVertexWithMaximumDegree() {
+  int maxDegreeVertex = 0;
+  int maxDegree = adjList[maxDegreeVertex].size();
+  for (int i=0; i<numberOfVertices; ++i) {
+    if (adjList[i].size() > maxDegree) {
+      maxDegreeVertex = i;
+      maxDegree = adjList[maxDegreeVertex].size();
+    }
+  }
+  return maxDegreeVertex;
+}
+
+int Graph::maxDistantVertex(int u) {
+  vector<int> distanceFromU = getDistanceFromU(u);
+  int maxDistantVertex = u;
+  int maxDistance = distanceFromU[maxDistantVertex];
+  for (int i=0; i<numberOfVertices; ++i) {
+    if (distanceFromU[i] > maxDistance) {
+      maxDistantVertex = i;
+      maxDistance = distanceFromU[maxDistantVertex];
+    }
+  }
+  return maxDistantVertex;
+}
+
+int Graph::midVertex(int u, int v) {
+  vector<int> distanceFromU(numberOfVertices);
+  vector<int> parentVertex(numberOfVertices);
+  vector<bool> visited(numberOfVertices, false);
+  queue<int> BFSQueue;
+
+  visited[u] = true;
+  distanceFromU[u] = 0;
+  parentVertex[u] = -1;
+  BFSQueue.push(u);
+
+  while (!BFSQueue.empty()) {
+    int top = BFSQueue.front();
+    BFSQueue.pop();
+
+    for (vector<int>::iterator it=adjList[top].begin(); it!=adjList[top].end(); ++it) {
+      if (!visited[*it]) {
+        visited[*it] = true;
+        distanceFromU[*it] = distanceFromU[top] + 1;
+        parentVertex[*it] = top;
+        BFSQueue.push(*it);
+      }
+    }
+    if (visited[v]) {
+      break;
+    }
+  }
+
+  int midDistance = distanceFromU[v]/2;
+  int midVertex = v;
+  while (midDistance--) {
+    midVertex = parentVertex[midVertex];
+  }
+  return midVertex;
+}
+
+pair<int,int> Graph::fourSweep() {
+  int r1 = getVertexWithMaximumDegree();
+  int a1 = maxDistantVertex(r1);
+  int b1 = maxDistantVertex(a1);
+  int r2 = midVertex(a1, b1);
+  int a2 = maxDistantVertex(r2);
+  int b2 = maxDistantVertex(a2);
+  int u = midVertex(a2, b2);
+  int lb = min(computeEccentricity(a1), computeEccentricity(a2));
+  return make_pair(lb, u);
 }
